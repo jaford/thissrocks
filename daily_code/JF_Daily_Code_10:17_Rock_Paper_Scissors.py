1a7f12432eef1e306e78ce375f89b1f1710c3ef2
James Ford <jamesford3@gmail.com>
2022-11-05 23:58:51 -0600

    adding updated rock paper scissors

--- daily_code/JF_Daily_Code_10:17_Rock_Paper_Scissors.py
+++ daily_code/JF_Daily_Code_10:17_Rock_Paper_Scissors.py
@@ -1,54 +1,54 @@
-#this is a rock, paper, scissors game played between one user and the computer
-
 import random
 
-def get_computer_choice():
-    ramdom_number = random.randint(1, 3)
 
-    #create a dicitionary with 3 items
-    options = {1: 'rock', 2:'paper', 3:'scissors'}
-    computers_choice = options[ramdom_number]
+# returns either 'rock', 'paper' or 'scissors'
+def get_computers_choice():
+    random_number = random.randint(1, 3)
+
+    options = {1: 'rock', 2: 'paper', 3: 'scissors'}
+    computers_choice = options[random_number]
 
     return computers_choice
 
-computers_choice =get_computer_choice()
-print(computers_choice)
 
+# take user input and return it
 def get_user_input():
-    user_input = input("Enter rock, paper or scissors ")
-    #Converting the user input into lowercase
+    user_input = input('Enter rock/paper/scissors: ')
+
+    # converting the user input to lowercase
     user_input = user_input.lower()
+
     return user_input
-result = get_user_input()
-print(result)
 
+
+# return either 'win', 'lose' or 'draw'
 def get_result(user_pick, computer_pick):
-
-    #condition for user to draw
     if computer_pick == user_pick:
         return 'draw'
 
-#condition for user to win
     elif user_pick == 'paper' and computer_pick == 'rock':
-        return win
+        return 'win'
     elif user_pick == 'rock' and computer_pick == 'scissors':
-        return win
+        return 'win'
     elif user_pick == 'scissors' and computer_pick == 'paper':
-        return win
+        return 'win'
 
-    #all other conditions result to loss
     else:
         return 'lose'
 
-#oustoed of the function
-computer_pick = get_computer_choice()
-while True:
-    user_pick = get_user_input()
-    if user_pick in ['rock','paper','scissors']:
-        break
-result = get_result(user_pick, computer_pick)
 
-#printing result
+# outside of the function
+computer_pick = get_computers_choice()
+
+# get input until user enters 'rock', 'paper' or scissors
+while True:
+    users_pick = get_user_input()
+    if users_pick in ['rock', 'paper', 'scissors']:
+        break
+
+result = get_result(users_pick, computer_pick)
+
+# printing result
 print(f"Computer's pick: {computer_pick}")
-print(f"Your pick: {user_pick}")
+print(f"Your pick: {users_pick}")
 print(f"You {result}")
