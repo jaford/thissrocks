e40eabcdd62e98e088b5315cc538ad32a3c773d4
Hunter Pimparatana <hunterpimparatana@gmail.com>
2023-02-23 15:05:33 -0700

    Logic for adding list to dictionaries to have a more condensed code! Possibly classes?

--- programs/scale_generator/chord_test.py
+++ programs/scale_generator/chord_test.py
@@ -12,6 +12,7 @@ VI  = minor
 VI  = major 
 VII = major
 
+Also attempting to append these list into a single dictionary!
 '''
 majorKeyChords = [
         'Major', 
@@ -21,6 +22,16 @@ majorKeyChords = [
         'Major',
         'Minor',
         'Diminished'
+]
+
+minorKeyChords = [
+        'Minor',
+        'Diminished', 
+        'Major', 
+        'Minor', 
+        'Minor', 
+        'Major', 
+        'Major'
 ]
 
 aMajorKeyNotes = [        
@@ -33,11 +44,40 @@ aMajorKeyNotes = [
         'G#'
 ]
 
+aMinorKeyNotes = [        
+        'A', 
+        'B', 
+        'C',
+        'D',
+        'E',
+        'F',
+        'G'
+]
+
 majorInterval = [2, 2, 1, 2, 2, 2, 1]
 
+
+'''
+Example:
+Values = [18, 20, 25, 29, 30]
+Details = dict({"Age": Values})
+print(Details)
+'''
+
+new_dict = dict(
+        {
+                'majorKeyChords': aMajorKeyNotes,
+                'minorKeyChords': aMinorKeyNotes
+        }
+)
+print(new_dict)
+print(type(new_dict))
+for x in new_dict:
+  print(x)
+  print(type(x))
+  notes = new_dict[x]
+  print(notes)
+  print(type(notes))
+
 majorChordList = '\n'.join('{} {}'.format(aMajorKeyNotes, majorKeyChords) for aMajorKeyNotes, majorKeyChords in zip(aMajorKeyNotes, majorKeyChords))
-
 print(majorChordList)
-
-
-
