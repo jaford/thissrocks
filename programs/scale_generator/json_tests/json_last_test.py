cdd68c542c31bc8b7b6c923ce417bca79a2c1f60
Hunter Pimparatana <hunterpimparatana@gmail.com>
2023-03-09 13:51:01 -0700

    removed hard coded dictionaries and list. Implemented function to parse JSON to three dictionaries that are callable in the global file. Also moved .py files in a test script directory to clean up.

--- programs/scale_generator/json_tests/json_last_test.py
+++ programs/scale_generator/json_tests/json_last_test.py
@@ -6,10 +6,10 @@ def parseData(data):
         notes = {}
         sharpNotes  = data['Sharps']
         flatNotes   = data['Flats']
-        notes['Shaprs'] = sharpNotes
-        notes['Flats'] = flatNotes
-        pprint.pprint(notes)
-        print(type(notes))
+        notes['Sharps'] = sharpNotes
+        notes['Flats']  = flatNotes
+        # pprint.pprint(notes)
+        # print(type(notes))
     if ('majorIntervals' and 'minorIntervals' and 'dorianIntervals' and 'phrygianIntervals' and 'lydianIntervals' and 'mixolydianIntervals' and 'locrianIntervals' and 'majorPentatonics' and 'minorPentatonics') in data:
         scaleNames = {}
         majorIntervals      = data['majorIntervals']
@@ -30,8 +30,8 @@ def parseData(data):
         scaleNames['locrian']           = locrianIntervals
         scaleNames['major pentatonic']  = majorPentatonics
         scaleNames['minor pentatonic']  = minorPentatonics
-        pprint.pprint(scaleNames)
-        print(type(scaleNames))
+        # pprint.pprint(scaleNames)
+        # print(type(scaleNames))
     if ( 'major' and 'minor' and 'dorian' and 'phrygian' and 'liydian' and 'mixoliydian' and 'locrian') in data:
         chordsInKeys = {}
         majorKeyChords      = data['major']
@@ -48,8 +48,8 @@ def parseData(data):
         chordsInKeys['liydian']     = lydianKeyChords
         chordsInKeys['mixoliydian'] = mixolydianKeyChords
         chordsInKeys['locrian']     = locrianKeyChords
-        pprint.pprint(chordsInKeys)
-        print(type(chordsInKeys))
+        # pprint.pprint(chordsInKeys)
+        # print(type(chordsInKeys))
 
     return notes, scaleNames, chordsInKeys
 
@@ -58,4 +58,45 @@ data = json.load(fileRead)
 print(data)
 print(type(data))
 notes, scaleNames, chordsInKeys = parseData(data)
+
+print('Here are the notes. {}'.format(notes))
+testVar = 'sharp'
+if 'Sharps' in notes:
+    note = notes['Sharps']
+    print(note)
+else: 
+    print('Error 1')
+
+if 'Flats' in notes:
+    note = notes['Flats']
+    print(note)
+else: 
+    print('Error 2')
+
+if testVar == 'sharp':
+    note = notes['Sharps']
+    print(note)
+else: 
+    print('Error 3')
+
+
+'''
+JUST TESTING THINGS OUT
+userInput = input('Enter "Major": ').lower()
+
+if userInput == 'major' in scaleNames:
+    print('You have enterd {}'.format(userInput))
+else: 
+    print('You did not read what I asked you to do! :(')
+
+userInput2 = input('Do you want to see the list of notes of Flats? (Y/N)').lower()
+
+if userInput2 == 'y':
+    scaleView = notes['Flats']
+    print('Here are your flats that you wanted.\n{}\nuwu'.format(scaleView))
+else: 
+    print('Entered incorrect value: {}'.format(userInput2))
+'''
+
+
 fileRead.close()
--- programs/scale_generator/main.py
+++ programs/scale_generator/main.py
@@ -1,172 +1,7 @@
 '''
 The creation of looping through the notes was done by Kai! I changed a section where you can have it diveded by groups of 5 and 7 note scales. 
 I have only included a handful of scales for the sake of simplifying. Possibly add more in the future. 
-'''
-import colorlog
-import os
-import sys, signal
-import json
-from termios import tcflush, TCIFLUSH
 
-def parseData(data):
-    if ('Sharps' and 'Flats') in data:
-        notes = {}
-        sharpNotes  = data['Sharps']
-        flatNotes   = data['Flats']
-        notes['Shaprs'] = sharpNotes
-        notes['Flats'] = flatNotes
-    if ('majorIntervals' and 'minorIntervals' and 'dorianIntervals' and 'phrygianIntervals' and 'lydianIntervals' and 'mixolydianIntervals' and 'locrianIntervals' and 'majorPentatonics' and 'minorPentatonics') in data:
-        scaleNames = {}
-        majorIntervals      = data['majorIntervals']
-        minorIntervals      = data['minorIntervals']
-        dorianIntervals     = data['dorianIntervals']
-        phrygianIntervals   = data['phrygianIntervals']
-        lydianIntervals     = data['lydianIntervals']
-        mixolydianIntervals = data['mixolydianIntervals']
-        locrianIntervals    = data['locrianIntervals']
-        majorPentatonics    = data['majorPentatonics']   
-        minorPentatonics    = data['minorPentatonics']  
-        scaleNames['major']             = majorIntervals
-        scaleNames['minor']             = minorIntervals
-        scaleNames['dorian']            = dorianIntervals
-        scaleNames['phrygian']          = phrygianIntervals
-        scaleNames['liydian']           = lydianIntervals
-        scaleNames['mixoliydian']       = mixolydianIntervals
-        scaleNames['locrian']           = locrianIntervals
-        scaleNames['major pentatonic']  = majorPentatonics
-        scaleNames['minor pentatonic']  = minorPentatonics
-    if ( 'major' and 'minor' and 'dorian' and 'phrygian' and 'liydian' and 'mixoliydian' and 'locrian') in data:
-        chordsInKeys = {}
-        majorKeyChords      = data['major']
-        minorKeyChords      = data['minor']
-        dorianKeyChords     = data['dorian']
-        phrygianKeyChords   = data['phrygian']
-        lydianKeyChords     = data['liydian']
-        mixolydianKeyChords = data['mixoliydian']
-        locrianKeyChords    = data['locrian']
-        chordsInKeys['major']       = majorKeyChords
-        chordsInKeys['minor']       = minorKeyChords
-        chordsInKeys['dorian']      = dorianKeyChords
-        chordsInKeys['phrygian']    = phrygianKeyChords
-        chordsInKeys['liydian']     = lydianKeyChords
-        chordsInKeys['mixoliydian'] = mixolydianKeyChords
-        chordsInKeys['locrian']     = locrianKeyChords
-
-    return notes, scaleNames, chordsInKeys
-    
-def scaleIntervals(userScaleInput, scaleNames):
-    scale = userScaleInput.lower()
-    while True:
-        if scale == 'major' in scaleNames or scale == 'ionian' in scaleNames:
-            intervals = majorIntervals
-            break
-        elif scale == 'minor' in scaleNames or scale == 'aeolian' in scaleNames:
-            intervals = minorIntervals
-            break
-        elif scale == 'dorian' in scaleNames:
-            intervals = dorianIntervals
-            break
-        elif scale == 'phrygian' in scaleNames:
-            intervals = phrygianIntervals
-            break
-        elif scale == 'lydian'in scaleNames:
-            intervals = lydianIntervals
-            break
-        elif scale == 'mixolydian'in scaleNames:
-            intervals = mixolydianIntervals 
-            break
-        elif scale == 'locrian'in scaleNames:
-            intervals = locrianIntervals    
-            break
-        elif scale == 'major pentatonic' in scaleNames or scale == 'major-pentatonic' in scaleNames:
-            intervals = majorPentatonics 
-            break
-        elif scale == 'minor pentatonic' in scaleNames or scale == 'minor-pentatonic' in scaleNames:
-            intervals = minorPentatonics          
-        else:
-            print('This "{}" scale does not exist or has not been added!'.format(userNoteInput))
-            break
-
-    return intervals
-
-def scaleAppending(scaleView, note, scaleLength):
-    scale = [] # Creating an empty list
-    scale.append(note) # add user inputted note to list
-    for i in range(scaleLength - 1):
-        note = scaleView[(scaleView.index(note) + intervals[i]) % 12]
-        scale.append(note)
-
-    return scale
-
-def findScale(userNoteInput, userScaleInput, intervals):
-
-    if userSharpOrFlat == 'sharp':
-        scaleView = sharpNotes
-    elif userSharpOrFlat == 'flat':
-        scaleView = flatNotes
-    else: 
-        print('Entered incorrect value: {}'.format(userSharpOrFlat))
-        
-    note = userNoteInput
-    scaleLength = len(intervals)
-    if scaleLength == 5:
-        scale = scaleAppending(scaleView, note, scaleLength)
-    elif scaleLength == 7:
-        scale = scaleAppending(scaleView, note, scaleLength)
-    else:
-        print('If you got here I would be very surprized! But here is your condition that would get you here! {}'.format(scaleLength))
-
-    return scale
-
-def chordsList(scale, key):
-    chordAppending = '\n'.join('{} {}'.format(scale, key) for scale, key in zip(scale, key))
-    return chordAppending 
-
-def findChords(scale, intervals, userScaleInput, chordsInKeys):
-    keySignatures = userScaleInput.lower() 
-    while True:
-        if keySignatures == 'major' in chordsInKeys or keySignatures == 'ionian' in chordsInKeys:
-            key = chordsInKeys['major']
-            chords = chordsList(scale, key)
-            break
-        elif keySignatures == 'minor' in chordsInKeys or keySignatures == 'aeolian' in chordsInKeys:
-            key = chordsInKeys['minor']
-            chords = chordsList(scale, key)
-            break
-        elif keySignatures == 'dorian'in chordsInKeys:
-            key = chordsInKeys['dorian']
-            chords = chordsList(scale, key)
-            break
-        elif keySignatures == 'phrygian'in chordsInKeys:
-            key = chordsInKeys['phrygian']
-            chords = chordsList(scale, key)
-            break
-        elif keySignatures == 'lydian'in chordsInKeys:
-            key = chordsInKeys['lydian']
-            chords = chordsList(scale, key)
-            break
-        elif keySignatures == 'mixolydian'in chordsInKeys:
-            key = chordsInKeys['mixolydian']
-            chords = chordsList(scale, key)
-            break
-        elif keySignatures == 'locrian'in chordsInKeys:
-            key = chordsInKeys['locrian']
-            chords = chordsList(scale, key)
-            break
-        else:
-            print('If you got here I would be very surprized! But here is your condition that would get you here! {}'.format(keySignatures))
-            
-    return chords
-# List of global varibles possibly used once in a class?
-# userSharpOrFlat = None
-# userScaleInput  = None
-# userNoteInput   = None
-# sharpNotes      = None
-# flatNotes       = None
-# intervals       = None
-# scale           = None
-# notes           = None
-'''
 Note:
 Wholestep   = 2
 Halfstep    = 1
@@ -190,7 +25,7 @@ IV  = minor
 VI  = minor
 VI  = major 
 VII = major–
-'''
+
 sharpNotes              = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'] # All notes in western music in sharps 
 flatNotes               = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'] # All notes in western music in flats 
 majorIntervals          = [2, 2, 1, 2, 2, 2, 1] # major scale by intervals W W H W W W H
@@ -241,7 +76,171 @@ notes = dict(
         'Flats': flatNotes
     }
 )
+'''
+import colorlog
+import os
+import sys, signal
+import json
+from termios import tcflush, TCIFLUSH
 
+def parseData(data):
+    if ('Sharps' and 'Flats') in data:
+        notes = {}
+        sharpNotes  = data['Sharps']
+        flatNotes   = data['Flats']
+        notes['Sharps'] = sharpNotes
+        notes['Flats']  = flatNotes
+    if ('majorIntervals' and 'minorIntervals' and 'dorianIntervals' and 'phrygianIntervals' and 'lydianIntervals' and 'mixolydianIntervals' and 'locrianIntervals' and 'majorPentatonics' and 'minorPentatonics') in data:
+        scaleNames = {}
+        majorIntervals      = data['majorIntervals']
+        minorIntervals      = data['minorIntervals']
+        dorianIntervals     = data['dorianIntervals']
+        phrygianIntervals   = data['phrygianIntervals']
+        lydianIntervals     = data['lydianIntervals']
+        mixolydianIntervals = data['mixolydianIntervals']
+        locrianIntervals    = data['locrianIntervals']
+        majorPentatonics    = data['majorPentatonics']   
+        minorPentatonics    = data['minorPentatonics']  
+        scaleNames['major']             = majorIntervals
+        scaleNames['minor']             = minorIntervals
+        scaleNames['dorian']            = dorianIntervals
+        scaleNames['phrygian']          = phrygianIntervals
+        scaleNames['liydian']           = lydianIntervals
+        scaleNames['mixoliydian']       = mixolydianIntervals
+        scaleNames['locrian']           = locrianIntervals
+        scaleNames['major pentatonic']  = majorPentatonics
+        scaleNames['minor pentatonic']  = minorPentatonics
+    if ( 'major' and 'minor' and 'dorian' and 'phrygian' and 'liydian' and 'mixoliydian' and 'locrian') in data:
+        chordsInKeys = {}
+        majorKeyChords      = data['major']
+        minorKeyChords      = data['minor']
+        dorianKeyChords     = data['dorian']
+        phrygianKeyChords   = data['phrygian']
+        lydianKeyChords     = data['liydian']
+        mixolydianKeyChords = data['mixoliydian']
+        locrianKeyChords    = data['locrian']
+        chordsInKeys['major']       = majorKeyChords
+        chordsInKeys['minor']       = minorKeyChords
+        chordsInKeys['dorian']      = dorianKeyChords
+        chordsInKeys['phrygian']    = phrygianKeyChords
+        chordsInKeys['liydian']     = lydianKeyChords
+        chordsInKeys['mixoliydian'] = mixolydianKeyChords
+        chordsInKeys['locrian']     = locrianKeyChords
+
+    return notes, scaleNames, chordsInKeys
+    
+def scaleIntervals(userScaleInput, scaleNames):
+    scale = userScaleInput.lower()
+    while True:
+        if scale == 'major' in scaleNames or scale == 'ionian' in scaleNames:
+            intervals = scaleNames['major']
+            break
+        elif scale == 'minor' in scaleNames or scale == 'aeolian' in scaleNames:
+            intervals = scaleNames['minor']
+            break
+        elif scale == 'dorian' in scaleNames:
+            intervals = scaleNames['dorian']
+            break
+        elif scale == 'phrygian' in scaleNames:
+            intervals = scaleNames['phrygian']
+            break
+        elif scale == 'lydian'in scaleNames:
+            intervals = scaleNames['lydian']
+            break
+        elif scale == 'mixolydian'in scaleNames:
+            intervals = scaleNames['mixolydian']
+            break
+        elif scale == 'locrian'in scaleNames:
+            intervals = scaleNames['locrian']   
+            break
+        elif scale == 'major pentatonic' in scaleNames or scale == 'major-pentatonic' in scaleNames:
+            intervals = scaleNames['major pentatonic'] 
+            break
+        elif scale == 'minor pentatonic' in scaleNames or scale == 'minor-pentatonic' in scaleNames:
+            intervals = scaleNames['minor pentatonic']         
+        else:
+            print('This "{}" scale does not exist or has not been added!'.format(userNoteInput))
+            break
+
+    return intervals
+
+def scaleAppending(scaleView, note, scaleLength):
+    scale = [] # Creating an empty list
+    scale.append(note) # add user inputted note to list
+    for i in range(scaleLength - 1):
+        note = scaleView[(scaleView.index(note) + intervals[i]) % 12]
+        scale.append(note)
+
+    return scale
+
+def findScale(userNoteInput, userSharpOrFlat, intervals, notes):
+
+    if userSharpOrFlat == 'sharp':
+        scaleView = notes['Sharps']
+    elif userSharpOrFlat == 'flat':
+        scaleView = notes['flats']
+    else: 
+        print('Entered incorrect value: {}'.format(userSharpOrFlat))
+        
+    note = userNoteInput
+    scaleLength = len(intervals)
+    if scaleLength == 5:
+        scale = scaleAppending(scaleView, note, scaleLength)
+    elif scaleLength == 7:
+        scale = scaleAppending(scaleView, note, scaleLength)
+    else:
+        print('If you got here I would be very surprized! But here is your condition that would get you here! {}'.format(scaleLength))
+
+    return scale
+
+def chordsList(scale, key):
+    chordAppending = '\n'.join('{} {}'.format(scale, key) for scale, key in zip(scale, key))
+    return chordAppending 
+
+def findChords(scale, intervals, userScaleInput, chordsInKeys):
+    keySignatures = userScaleInput.lower() 
+    while True:
+        if keySignatures == 'major' or keySignatures == 'ionian':
+            key = chordsInKeys['major']
+            chords = chordsList(scale, key)
+            break
+        elif keySignatures == 'minor' or keySignatures == 'aeolian':
+            key = chordsInKeys['minor']
+            chords = chordsList(scale, key)
+            break
+        elif keySignatures == 'dorian':
+            key = chordsInKeys['dorian']
+            chords = chordsList(scale, key)
+            break
+        elif keySignatures == 'phrygian':
+            key = chordsInKeys['phrygian']
+            chords = chordsList(scale, key)
+            break
+        elif keySignatures == 'lydian':
+            key = chordsInKeys['lydian']
+            chords = chordsList(scale, key)
+            break
+        elif keySignatures == 'mixolydian':
+            key = chordsInKeys['mixolydian']
+            chords = chordsList(scale, key)
+            break
+        elif keySignatures == 'locrian':
+            key = chordsInKeys['locrian']
+            chords = chordsList(scale, key)
+            break
+        else:
+            print('This is not a supported scale or mode in this program yet! {}'.format(keySignatures))
+            
+    return chords
+# List of global varibles possibly used once in a class?
+# userSharpOrFlat = None
+# userScaleInput  = None
+# userNoteInput   = None
+# sharpNotes      = None
+# flatNotes       = None
+# intervals       = None
+# scale           = None
+# notes           = None
 while True:
     intro_text = '\n----♪ Welcome to my Scale Program! ♪----\n\n----How to use----\n'\
         'Step 1: Enter how you want to see the notes! Either in sharps (#) or flats (b)\n'\
@@ -257,6 +256,8 @@ while True:
         try:
             tcflush(sys.stdin, TCIFLUSH)
             fileRead = open('data.json')
+            # This absolute path is used to debug in VSCODE for whatever reason
+            # fileRead = open('/Users/hunterpimparatana/Documents/practice_code/source/thissrocks/programs/scale_generator/data.json')
             data = json.load(fileRead)
             notes, scaleNames, chordsInKeys = parseData(data)
 
@@ -277,13 +278,13 @@ while True:
             userStart = input('Are you ready to continue? (Y/N)\n').lower()
             if userStart == 'y':
                 intervals   = scaleIntervals(userScaleInput, scaleNames)
-                scale       = findScale(userNoteInput, userScaleInput, intervals)
+                scale       = findScale(userNoteInput, userSharpOrFlat, intervals, notes)
                 if isinstance(scale, list):
                     scale = ', '.join(scale)
                     print('\nHere are the notes in the {} {} scale: \n{}\n'.format(userNoteInput, userScaleInput, scale))
                 userStartChords = input('Do you want to see the Chords? (Y/N)\n').lower()
                 if userStartChords == 'y':
-                    scale   = findScale(userNoteInput, userScaleInput, intervals) # Calling this again in order to get the scale as a list
+                    scale   = findScale(userNoteInput, userSharpOrFlat, intervals, notes) # Calling this again in order to get the scale as a list
                     chords  = findChords(scale, intervals, userScaleInput, chordsInKeys)
                     print('\nHere are your chrods in the {} {} scale!\n{}\n----Enter another scale----\n'.format(userNoteInput, userScaleInput, chords))
                 elif userStartChords == 'n' or userStartChords == 'q':
