69dc7a32ec487064435523168286357acbf2e50d
Hunter Pimparatana <hunterpimparatana@gmail.com>
2023-03-14 17:23:51 -0600

    Saving work for learning how to import functions from a sibling directory to clean up code.

--- programs/scale_generator/testScripts/folderTest1/callingFunctionNewDirectory.py
+++ programs/scale_generator/testScripts/folderTest1/callingFunctionNewDirectory.py
@@ -1,8 +1,15 @@
-from folderTest2 import *
+# from folderTest2 import subtration, printTest # pylint: disable=import-error
 import sys
 
-sys.path.append(0, '/Users/hunterpimparatana/Documents/practice_code/source/thissrocks/programs/scale_generator/testScripts/folderTest2')
+# sys.path.append(0, '/thissrocks/programs/scale_generator/testScripts/folderTest2')
+# sys.path.instert(0, '')
 
+# sys.path.append('/Users/hunterpimparatana/Documents/practice_code/source/thissrocks/programs/scale_generator/testScripts/folderTest1')
+# sys.path.insert(1, '/Users/hunterpimparatana/Documents/practice_code/source/thissrocks/programs/scale_generator/testScripts/folderTest1')
+# sys.path.append('/scale_generator/testScripts/folderTest2')
+sys.path.append('..')
+
+from folderTest2.folderFunctions import subtration, printTest
 result = subtration(20, 5)
 printTest()
 print('Here is your result: "{}"'.format(result))
