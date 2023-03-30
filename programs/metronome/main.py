0817edd2116f33527053833090ec3f9b79f37021
Hunter Pimparatana <hunterpimparatana@gmail.com>
2023-03-30 14:46:16 -0600

    Made main.py much smaller by adding a additional function for user inputs. This creates a cleaner code base and a better way to organize the code! Need to see if there is a way to always run as sudo for the future.

--- programs/metronome/main.py
+++ programs/metronome/main.py
@@ -22,29 +22,28 @@ from termios import tcflush, TCIFLUSH
 from playsound import playsound
 
 sys.path.append('..')
-from functions.metronomeCounter import metronomeCounter, metronomeValues
+from functions.metronomeCounter import metronomeCounter, metronomeValues, userInputs
 from functions.metronomeInfo    import metronomeHelp, metronomeMath
 from functions.deleteLastLine   import deleteLastLine
 
-user_bpm_input  = None
-user_note_val   = None
+bpmInput  = None
+NoteVal   = None
 user_time_sign  = None 
-sec_val         = None
-beat_val        = None 
+secVal         = None
+beatVal        = None 
 time_sign_list  = [4, 8, 16, 32] # Probably get rid of this list since I do not really to access it. Maybe the other branch can over engineer it! 
+introText = '\n----♪ Welcome to my Metronome Program! ♪----\n'\
+    '----How to use----\nStep 1: Start by entering a BPM. (This can be any positive value)\n'\
+    'Step 2: Choose a note value bellow: \n"Quarter"   = Counts of 4ths\n"Eighth"    = Counts in'\
+    ' 8ths\n"Sixteenth" = Counts in 16ths\n"Thirty-second" = Counts in 32nds\n'\
+    'Step 3: Enter the amount of beats in each measure. (This can be any whole number)\n'\
+    '\n----Extra Info----\nYou can even type in "help" at anypoint to show extra instructions!'\
+    'To restart program press "CTRL + C" or "Q"\n'\
+    'May need to press "Q" multiple times to quit \n'\
+    'This is a current bug and it is being addressed! (01/21/2023)\n'\
+    'To access some more info of the current entered fields, type in "!info" after entering all the values\n'  
 
-while True:
-    introText = '\n----♪ Welcome to my Metronome Program! ♪----\n'\
-        '----How to use----\nStep 1: Start by entering a BPM. (This can be any positive value)\n'\
-        'Step 2: Choose a note value bellow: \n"Quarter"   = Counts of 4ths\n"Eighth"    = Counts in'\
-        ' 8ths\n"Sixteenth" = Counts in 16ths\n"Thirty-second" = Counts in 32nds\n'\
-        'Step 3: Enter the amount of beats in each measure. (This can be any whole number)\n'\
-        '\n----Extra Info----\nYou can even type in "help" at anypoint to show extra instructions!'\
-        'To restart program press "CTRL + C" or "Q"\n'\
-        'May need to press "Q" multiple times to quit \n'\
-        'This is a current bug and it is being addressed! (01/21/2023)\n'\
-        'To access some more info of the current entered fields, type in "!info" after entering all the values\n'\
-
+while True:                              
     print(introText)
     userStart = input('Are you ready to start? (Y/N)\n').lower().strip()
     if userStart == 'y':
@@ -56,45 +55,20 @@ while True:
                 deleteLastLine(lineAmount)
         while True:
             try:
-                # Attempted to clear keyboard inputs by setting the variable to None. Did not work :(
-                tcflush(sys.stdin, TCIFLUSH)
-                user_bpm_input = input('Enter BPM: ').lower()
-                if user_bpm_input == 'q':
-                    print('You have quit the program.\n')       
-                    exit()
-                if user_bpm_input == 'help':
-                    help(metronomeHelp)
-                    break
+                beatNum, noteVal, bpmInput = userInputs()
 
-                user_note_val = input('Enter a note value: ').lower()
-                if user_note_val == 'q':
-                    print('You have quit the program.\n')       
-                    exit()
-                if user_note_val == 'help':
-                    help(metronomeHelp)
-                    break
-                    
-                user_beat_num = input('Enter your how many beats per measure: ').lower()
-                beat_num = int(user_beat_num)
-                if user_beat_num == 'q':
-                    print('You have quit the program.\n')       
-                    exit()
-                if user_beat_num == 'help':
-                    help(metronomeHelp)
-                    break
-
-                print('\nHere are your values!\nBPM: {}\nNote Value: {}\nNumber of beats per measure: {}'.format(user_bpm_input, user_note_val, user_beat_num))
-                user_start = input('Are you ready to continue?(Y/N)\n').lower()
+                print('\nHere are your values!\nBPM: {}\nNote Value: {}\nNumber of beats per measure: {}'.format(bpmInput, noteVal, beatNum))
+                userStart = input('Are you ready to continue?(Y/N)\n').lower()
                 if isinstance(userStart, str):
                     lineAmount = len(userStart.splitlines())
                     deleteLastLine(lineAmount + 3)
-                if user_start == 'y':
-                    sec_val, beat_val   = metronomeValues(user_note_val, user_bpm_input)
-                    tstep, tnext        = metronomeCounter(user_bpm_input, user_note_val, beat_num, beat_val, sec_val)
-                elif user_start == '!info':
-                    sec_val, beat_val   = metronomeValues(user_note_val, user_bpm_input)
-                    metronomeMath(user_bpm_input, user_note_val, sec_val, beat_num, beat_val)
-                elif user_start == 'n':
+                if userStart == 'y':
+                    secVal, beatVal = metronomeValues(noteVal, bpmInput)
+                    tstep, tnext    = metronomeCounter(bpmInput, noteVal, beatNum, beatVal, secVal)
+                elif userStart == '!info':
+                    secVal, beatVal   = metronomeValues(noteVal, bpmInput)
+                    metronomeMath(bpmInput, noteVal, secVal, beatNum, beatVal)
+                elif userStart == 'n':
                     print('\n----You have quit the metronome!----\n')
                     exit()
                 else:
@@ -102,11 +76,14 @@ while True:
 
             except Exception as err: 
                 colorlog.error('\nA error occured --> : "{}"\n'.format(err))
-                colorlog.warning('You have entered a non-supported format for BPM: {}\n'.format(user_bpm_input))
-                colorlog.warning('You have entered a non-supported format Note Value: {}\n'.format(user_note_val))
-                colorlog.warning('You have entered a non-supported format Beat Value: {}\n'.format(beat_val))
+                colorlog.warning('You have entered a non-supported format for BPM: {}\n'.format(bpmInput))
+                colorlog.warning('You have entered a non-supported format Note Value: {}\n'.format(noteVal))
+                colorlog.warning('You have entered a non-supported format Beat Value: {}\n'.format(beatNum))
                 pass
     elif userStart == 'n':
         print('\nYou have quit the program\n')
         exit()
+    else: 
+        colorlog.error('\nA error occured --> : "{}" is a non supported input.\n'.format(userStart))
+        break
 
