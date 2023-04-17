de421c0d2f064227743f8ef4d68a68784ad752e7
Hunter Pimparatana <hunterpimparatana@gmail.com>
2023-04-16 18:24:37 -0600

    Getting more familiar with calling classes and the functions within a class. May need to see how I can call a class on another file? Like a main.py file

--- programs/shortScripts/classCreation/classTest3.py
+++ programs/shortScripts/classCreation/classTest3.py
@@ -49,36 +49,121 @@ def parseData(data):
 
     return notes, scaleNames, chordsInKeys
 
+def jsonRead():
+    tcflush(sys.stdin, TCIFLUSH)
+    fileRead = open('data.json')
+    data = json.load(fileRead)
+    
+    return data
 
 class scales:
     #constructor function    
-    def __init__(self, notes={}, scaleNames={}, chordsInKeys={}):
-        self.notes = notes
-        self.scaleNames = scaleNames
-        self.chordsInKeys = chordsInKeys
+    def __init__(self, notes={}, scaleNames={}, chordsInKeys={}, intervals=[], chords=[]):
+        self.notes          = notes
+        self.scaleNames     = scaleNames
+        self.chordsInKeys   = chordsInKeys
+        self.intervals      = intervals
+        self.chords         = chords
+        self.note           = None
+        self.userInput      = None
 
-    def displayDicts(self):
-        if 'major' in scaleNames and 'major' in chordsInKeys:
-            intervals   = scaleNames.get('major')
-            chords      = chordsInKeys.get('major')
-            print('You are in the major scale\nMajor Scale intervals: {}\nChords in Scale: {}'.format(intervals, chords))
-        elif 'minor' in scaleNames and 'minor' in chordsInKeys:
-            intervals   = scaleNames.get('minor')
-            chords      = chordsInKeys.get('minor')
-            print('You are in the minor scale\nMinor Scale intervals: {}\nChords in Scale: {}'.format(intervals, chords))
+    def keyIntervals(self):
+        userInput = input('Select a scale that you want to use: ').lower()
+        if userInput == 'major' or userInput == 'ionian':
+            intervals   = scaleNames['major']
+            chords      = chordsInKeys['major']
+        elif userInput == 'minor' or userInput == 'aeolian':
+            intervals   = scaleNames['minor']
+            chords      = chordsInKeys['minor']
+        elif userInput == 'dorian' in scaleNames:
+            intervals   = scaleNames['dorian']
+            chords      = chordsInKeys['dorian']    
+        elif userInput == 'phrygian' in scaleNames:
+            intervals   = scaleNames['phrygian']
+            chords      = chordsInKeys['phrygian']    
+        elif userInput == 'lydian'in scaleNames:
+            intervals   = scaleNames['lydian']
+            chords      = chordsInKeys['lydian']    
+        elif userInput == 'mixolydian'in scaleNames:
+            intervals   = scaleNames['mixolydian']
+            chords      = chordsInKeys['mixolydian']    
+        elif userInput == 'locrian'in scaleNames:
+            intervals   = scaleNames['locrian']   
+            chords      = chordsInKeys['locrian']    
+        elif userInput == 'major pentatonic' or userInput == 'major-pentatonic':
+            intervals   = scaleNames['major pentatonic'] 
+            chords      = chordsInKeys['major pentatonic']    
+        elif userInput == 'minor pentatonic' or userInput == 'minor-pentatonic':
+            intervals   = scaleNames['minor pentatonic']     
+            chords      = chordsInKeys['minor pentatonic']    
         else:
-            print('Error\n')
+            print('\nThis is not a supported scale: "{}"'.format(userInput))
+        
         return intervals, chords
 
+    def scaleNotes(self, intervals):
+        userInput = input('Show Sharp or Flat? Enter "Sharp" or "Flat": ').lower()
+        note = input('Pick a note: ').strip()
+        if userInput == 'sharp':
+            scaleView = notes['Sharps']
+        elif userInput == 'flat':
+            scaleView = notes['Flats']
+        else:
+            print('\nThis is not a supported input: "{}"'.format(userInput))
+        
+        scaleLength = len(intervals)
+        scale = [] # Creating an empty list
+        scale.append(note) # add user inputed note to list
+        for i in range(scaleLength - 1):
+            note = scaleView[(scaleView.index(note) + intervals[i]) % 12]
+            scale.append(note)
 
-tcflush(sys.stdin, TCIFLUSH)
-fileRead = open('data.json')
-data = json.load(fileRead)
+        return scale
+
+    def displayScale(self, scale):
+        if isinstance(scale, list):
+            scaleStr = ', '.join(scale)       
+            print('\nHere is your scale!\nScale: {}\n'.format(scale))
+
+        return
+
+
+    '''
+    Test this later!
+    def chordsList(self, scale, key):
+        chordAppending = '\n'.join('{} {}'.format(scale, key) for scale, key in zip(scale, key))
+        return chordAppending 
+
+    def createChords(self):
+        
+        return
+
+    '''
+data = jsonRead()
 notes, scaleNames, chordsInKeys = parseData(data)
 
-majorScale = scales(notes, scaleNames, chordsInKeys)
-minorScale = scales(notes, scaleNames, chordsInKeys)
+# userStart = input('Are you ready to start? (Y/N)\n').lower().strip()
 
-majorScale.displayDicts()
-minorScale.displayDicts()
+# if userStart == 'y':
+createScale = scales(notes, scaleNames, chordsInKeys)
+intervals, chords = createScale.keyIntervals()
+scale = createScale.scaleNotes(intervals)
+createScale.displayScale(scale)
+
+
+
+# elif userStart == 'n' or userStart == 'q':
+#     print('You have quit the program.\n')
+#     exit()
+# else:
+#     print('You have entered a incorrect value: {}\nQuiting the program...'.format(userStart))
+#     exit()
+
+
+
+
+# minorScale = scales(notes, scaleNames, chordsInKeys)
+
+# Goes into function inside the class
+# minorScale.displayDicts()
 
