898812434005987c44a95a8905d65253edd57507
Hunter Pimparatana <hunterpimparatana@gmail.com>
2023-04-23 20:15:56 -0600

    Saving changes!

--- programs/shortScripts/emailSendTest/sendEmailwithData.py
+++ programs/shortScripts/emailSendTest/sendEmailwithData.py
@@ -1,11 +1,34 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
 def stringResult(restultDict):
+    emailSender = 'hunterpimparatana@gmail.com'
+    emailPassword = 'qvrewmnqekaqbivl'
+    emailReceiver = 'huntermagicman@live.com'
+
+    subject = 'Check out this test!'
+    body = 'Here is the result of your numbers: {}'.format(result)
+
+    em = EmailMessage()
+    em['From'] = emailSender
+    em['To'] = emailReceiver
+    em['Subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    # Will test this at a later time!
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(emailSender, emailPassword)
+        smtp.sendmail(emailSender, emailReceiver, em.as_string())
+
+
     if isinstance(restultDict, dict):
         print('\nHere is the results of your calculations bellow:\n')
-
-        data = ''
         for i, v in restultDict.items():
-            data += str(i + '\t', v)
-        print(data)
+            print(i + '\t', v)
+
     body ='\nHere is a example on how to display data onto a string. Here is test to put into this string.\nThis is one way I can fomat data into a email so I can send it to user & myself!\n'
     print(body)
     
@@ -30,12 +53,3 @@ restultDict['Addition']         = resultAdd
 restultDict['Subtraction']      = resultSub
 restultDict['Multiplcation']    = resultMulti
 restultDict['Division']         = resultDiv
-
-print(restultList)
-print(type(restultList))
-
-print(restultDict)
-print(type(restultDict))
-
-
-stringResult(restultDict)
