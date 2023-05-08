48c76e1956434a293e35ccb1453205fed00d54fe
Hunter Pimparatana <hunterpimparatana@gmail.com>
2023-05-07 20:30:24 -0600

    Saving my work. Noticed that it takes a long time to connect from time to time. No errors when I run it normaly when the script is on main.py
    I have the main connection and data collecting happening in the weatherData.py file. The async "thing" is apart of a function where I can return values or other things! :)

--- programs/weather/functions/weatherData.py
+++ programs/weather/functions/weatherData.py
@@ -1,54 +1,121 @@
 import python_weather
 import asyncio
+
+
 async def getWeather():
   # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
   async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
   # async with python_weather.Client(unit=python_weather.METRIC) as client:
+
     # fetch a weather forecast from a city
     weather = await client.get('Albuquerque')
     dateTime = weather.current.date
     currentDate = dateTime.strftime('%m/%d/%Y')
     currentTime = dateTime.strftime('%I:%M %p')
 
-    try:  
-      print('\nHere is the current stats for Albuquerque\nDate: {}\nTime: {}\n--------'.format(currentDate, currentTime))    
-      print(u'Temperature: {}\u00b0'.format(weather.current.temperature))
-      print(u'What it feels like: {}\u00b0'.format(weather.current.feels_like))
-      print(u'Humidity:        {}\u0025'.format(weather.current.humidity))
-      print(u'Visibility:      {}\u0025'.format(weather.current.visibility))
-      print(u'Precipitation:   {}\u0025'.format(weather.current.precipitation))
-      print('Wind speed:      {}mph'.format(weather.current.wind_speed))
-      print('Wind direction:  {}'.format(weather.current.wind_direction))
-      print('--------\n')    
+    # Noticed program will disconnect. Re-run program until a connection is sucure?
+    # Attempt to save the methods into callable objects from this function
+    cTemp = str(weather.current.temperature)
+    cFl   = str(weather.current.feels_like)
+    cHum  = str(weather.current.humidity)
+    cViz  = str(weather.current.visibility)
+    cPrec = str(weather.current.precipitation)
+    cWindS = str(weather.current.wind_speed)
+    cWindD = str(weather.current.wind_direction)
 
-      #  Noticed it disconnects. Re-run program until a connection is sucure?
-      # Attempt to save the methods into callable objects from this function
-      cTemp = str(weather.current.temperature)
-      cFl   = str(weather.current.feels_like)
-      cHum  = str(weather.current.humidity)
-      cViz  = str(weather.current.visibility)
-      cPrec = str(weather.current.precipitation)
-      cWindS = str(weather.current.wind_speed)
-      cWindD = str(weather.current.wind_direction)
+    # print('Here is the next three days forecast in Albuquerque from {}\n--------'.format(currentDate))    
+    # get the weather forecast for a few days
+    forcastDay = {
+      'forcastDate': [],
+      'futureTemp': [],
+      'highTemp': [],
+      'lowTemp': []
+    }
 
-      print('Here is the next three days forecast in Albuquerque from {}\n--------'.format(currentDate))    
-      # get the weather forecast for a few days
-      for forecast in weather.forecasts:
-        d = forecast.date
-        fTemp = str(forecast.temperature)
-        fLowTemp = str(forecast.lowest_temperature)
-        fHighTemp = str(forecast.highest_temperature)
-        forecastDate = d.strftime(f'%m/%d/%Y')
-        print('Date: {}'.format(forecastDate))
-        print(u'Average temperature: {}\u00b0'.format(forecast.temperature))
-        print(u'Lowest temperature:  {}\u00b0'.format(forecast.lowest_temperature))
-        print(u'Highest temperature: {}\u00b0\n'.format(forecast.highest_temperature))
-      print('--------\n')  
-      # hourly forecasts
-      for hourly in forecast.hourly:
-        print(f' --> {hourly!r}')
+    forcastHour = {
+      'forcastHour': [],
+      'temperature': [],
+      'description': []
+    }
 
-    except Exception as err: 
-      print('An error has occured: {}'.format(err))
+    for forecast in weather.forecasts:
+      d = forecast.date
+      forecastDate = d.strftime(f'%m/%d/%Y')
+      fTemp = str(forecast.temperature)
+      fLowTemp = str(forecast.lowest_temperature)
+      fHighTemp = str(forecast.highest_temperature)
+      forcastDay['forcastDate'].append(forecastDate)
+      forcastDay['futureTemp'].append(fTemp)
+      forcastDay['highTemp'].append(fLowTemp)
+      forcastDay['lowTemp'].append(fHighTemp)  
 
-asyncio.run(getWeather())
+    for hourly in forecast.hourly:
+      d = hourly.time
+      forecastDate = d.strftime(f'%I:%M %p')
+      hTemp = str(hourly.temperature)
+      hDescr = str(hourly.description)
+      forcastHour['forcastHour'].append(forecastDate)
+      forcastHour['temperature'].append(hTemp)
+      forcastHour['description'].append(hDescr)
+      print(f' --> {hourly!r}')
+
+    return cTemp, cFl, cHum, cViz, cPrec, cWindS, cWindD, fTemp, fLowTemp, fHighTemp, currentDate, currentTime, forcastDay, forcastHour
+
+'''
+USE THIS CODE AS REFERANCE TO FORMAT STRINGS IN THE OUTPUT
+LOOK AT THE PRINT STATEMENTS PLEASE
+
+# async def getWeather():
+#   # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+#   async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+#   # async with python_weather.Client(unit=python_weather.METRIC) as client:
+#     # fetch a weather forecast from a city
+#     weather = await client.get('Albuquerque')
+#     dateTime = weather.current.date
+#     currentDate = dateTime.strftime('%m/%d/%Y')
+#     currentTime = dateTime.strftime('%I:%M %p')
+
+#     try:  
+#       print('\nHere is the current stats for Albuquerque\nDate: {}\nTime: {}\n--------'.format(currentDate, currentTime))    
+#       print(u'Temperature: {}\u00b0'.format(weather.current.temperature))
+#       print(u'What it feels like: {}\u00b0'.format(weather.current.feels_like))
+#       print(u'Humidity:        {}\u0025'.format(weather.current.humidity))
+#       print(u'Visibility:      {}\u0025'.format(weather.current.visibility))
+#       print(u'Precipitation:   {}\u0025'.format(weather.current.precipitation))
+#       print('Wind speed:      {}mph'.format(weather.current.wind_speed))
+#       print('Wind direction:  {}'.format(weather.current.wind_direction))
+#       print('--------\n')    
+
+#       #  Noticed it disconnects. Re-run program until a connection is sucure?
+#       # Attempt to save the methods into callable objects from this function
+#       cTemp = str(weather.current.temperature)
+#       cFl   = str(weather.current.feels_like)
+#       cHum  = str(weather.current.humidity)
+#       cViz  = str(weather.current.visibility)
+#       cPrec = str(weather.current.precipitation)
+#       cWindS = str(weather.current.wind_speed)
+#       cWindD = str(weather.current.wind_direction)
+
+#       print('Here is the next three days forecast in Albuquerque from {}\n--------'.format(currentDate))    
+#       # get the weather forecast for a few days
+#       for forecast in weather.forecasts:
+#         d = forecast.date
+#         fTemp = str(forecast.temperature)
+#         fLowTemp = str(forecast.lowest_temperature)
+#         fHighTemp = str(forecast.highest_temperature)
+#         forecastDate = d.strftime(f'%m/%d/%Y')
+#         print('Date: {}'.format(forecastDate))
+#         print(u'Average temperature: {}\u00b0'.format(forecast.temperature))
+#         print(u'Lowest temperature:  {}\u00b0'.format(forecast.lowest_temperature))
+#         print(u'Highest temperature: {}\u00b0\n'.format(forecast.highest_temperature))
+#       print('--------\n')  
+#       # hourly forecasts
+#       for hourly in forecast.hourly:
+#         print(f' --> {hourly!r}')
+
+#     except Exception as err: 
+#       print('An error has occured: {}'.format(err))
+
+# asyncio.run(getWeather())
+
+'''
--- programs/weather/main/main.py
+++ programs/weather/main/main.py
@@ -5,63 +5,76 @@ import sys, signal
 sys.path.append('..')
 from datetime import datetime
 from termios import tcflush, TCIFLUSH
-from functions.dataCompile import dataFormating
-from functions.feh2celConv import feh2cel
-from functions.progressBar import loadBar
-from functions.sendEmail import sendMail
-# from functions.weatherData import getWeather
+# from functions.dataCompile import dataFormating
+# from functions.feh2celConv import feh2cel
+# from functions.progressBar import loadBar
+# from functions.sendEmail import sendMail
+from functions.weatherData import getWeather
 
 
-async def getWeather():
-  # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
-  async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
-  # async with python_weather.Client(unit=python_weather.METRIC) as client:
-    # fetch a weather forecast from a city
-    weather = await client.get('Albuquerque')
-    dateTime = weather.current.date
-    currentDate = dateTime.strftime('%m/%d/%Y')
-    currentTime = dateTime.strftime('%I:%M %p')
+# THIS IS COMMENTED OUT JUST IN CASE
+# async def getWeather():
+#   # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+#   async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+#   # async with python_weather.Client(unit=python_weather.METRIC) as client:
 
-    try: 
-      # Noticed program will disconnect. Re-run program until a connection is sucure?
-      # Attempt to save the methods into callable objects from this function
-      cTemp = str(weather.current.temperature)
-      cFl   = str(weather.current.feels_like)
-      cHum  = str(weather.current.humidity)
-      cViz  = str(weather.current.visibility)
-      cPrec = str(weather.current.precipitation)
-      cWindS = str(weather.current.wind_speed)
-      cWindD = str(weather.current.wind_direction)
+#     # fetch a weather forecast from a city
+#     weather = await client.get('Albuquerque')
+#     dateTime = weather.current.date
+#     currentDate = dateTime.strftime('%m/%d/%Y')
+#     currentTime = dateTime.strftime('%I:%M %p')
 
-      # print('Here is the next three days forecast in Albuquerque from {}\n--------'.format(currentDate))    
-      # get the weather forecast for a few days
-      forcastData = {
-        'forcast date': [],
-        'future temp': [],
-        'highest temp': [],
-        'lowest temp': []
-      }
-      for forecast in weather.forecasts:
-        d = forecast.date
-        forecastDate = d.strftime(f'%m/%d/%Y')
-        fTemp = str(forecast.temperature)
-        fLowTemp = str(forecast.lowest_temperature)
-        fHighTemp = str(forecast.highest_temperature)
-        forcastData[0].append(forecastDate)
-        forcastData[1].append(fTemp)
-        forcastData[2].append(fLowTemp)
-        forcastData[3].append(fHighTemp)
-        print(forcastData)
-      for hourly in forecast.hourly:
-        print(f' --> {hourly!r}')
 
-    except Exception as err: 
-      print('An error has occured: {}'.format(err))
+#     # Noticed program will disconnect. Re-run program until a connection is sucure?
+#     # Attempt to save the methods into callable objects from this function
+#     cTemp = str(weather.current.temperature)
+#     cFl   = str(weather.current.feels_like)
+#     cHum  = str(weather.current.humidity)
+#     cViz  = str(weather.current.visibility)
+#     cPrec = str(weather.current.precipitation)
+#     cWindS = str(weather.current.wind_speed)
+#     cWindD = str(weather.current.wind_direction)
 
-    return cTemp, cFl, cHum, cViz, cPrec, cWindS, cWindD, fTemp, fLowTemp, fHighTemp, forecastDate, currentDate, currentTime
+#     # print('Here is the next three days forecast in Albuquerque from {}\n--------'.format(currentDate))    
+#     # get the weather forecast for a few days
+#     forcastDay = {
+#       'forcastDate': [],
+#       'futureTemp': [],
+#       'highTemp': [],
+#       'lowTemp': []
+#     }
 
-# Use this to run main.
-asyncio.run(getWeather())
+#     forcastHour = {
+#       'forcastHour': [],
+#       'temperature': [],
+#       'description': []
+#     }
+
+#     for forecast in weather.forecasts:
+#       d = forecast.date
+#       forecastDate = d.strftime(f'%m/%d/%Y')
+#       fTemp = str(forecast.temperature)
+#       fLowTemp = str(forecast.lowest_temperature)
+#       fHighTemp = str(forecast.highest_temperature)
+#       forcastDay['forcastDate'].append(forecastDate)
+#       forcastDay['futureTemp'].append(fTemp)
+#       forcastDay['highTemp'].append(fLowTemp)
+#       forcastDay['lowTemp'].append(fHighTemp)  
+
+#     for hourly in forecast.hourly:
+#       d = hourly.time
+#       forecastDate = d.strftime(f'%I:%M %p')
+#       hTemp = str(hourly.temperature)
+#       hDescr = str(hourly.description)
+#       forcastHour['forcastHour'].append(forecastDate)
+#       forcastHour['temperature'].append(hTemp)
+#       forcastHour['description'].append(hDescr)
+#       print(f' --> {hourly!r}')
+
+#     return cTemp, cFl, cHum, cViz, cPrec, cWindS, cWindD, fTemp, fLowTemp, fHighTemp, currentDate, currentTime, forcastDay, forcastHour
+
+# # Use this to run main.
+cTemp, cFl, cHum, cViz, cPrec, cWindS, cWindD, fTemp, fLowTemp, fHighTemp, currentDate, currentTime, forcastDay, forcastHour = asyncio.run(getWeather())
 
 # TEST THIS LATER 
 # cTemp, cFl, cHum, cViz, cPrec, cWindS, cWindD, fTemp, fLowTemp, fHighTemp, forecastDate, currentDate, currentTime = asyncio.run(getweather())
