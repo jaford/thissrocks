7658d4fff7994d30f6a0be6d19c6660261fd8334
Hunter Pimparatana <hunterpimparatana@gmail.com>
2024-04-03 18:36:36 -0600

    This graph does not take strings but only intergers and floats it looks like. Find another graphing output!

--- programs/weather/functions/graphicRep.py
+++ programs/weather/functions/graphicRep.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from tabulate import tabulate
 
+# Not calling this function at this time...
 def daily_forecast_plot(daily_forecast):
         fig = go.figure()
         fig.add_trace(go.Bar(x=daily_forecast['forcastDate'], y=daily_forecast['highTemp'], name='High Temp'))
@@ -25,14 +26,23 @@ def plot_forcast_data(forcastDataList):
                         fig.show()
                 else:
                         continue
-        
-        print(forcastDataList)
-        print(type(forcastDataList))
 
         for forecast_data in forcastDataList:
                 print("Forecast Data:")
                 print(forecast_data)
                 print("\n" + "=" * 30 + "\n")  # Separator for better readability
+        
+        print('Displaying graph on a html page...')
+        fig = go.Figure()
+        fig.add_trace(go.Bar(x=forcastDataList['forcastDate'], y=forcastDataList['highTemp'], name='High Temp'))
+        fig.add_trace(go.Bar(x=forcastDataList['forcastDate'], y=forcastDataList['lowTemp'], name='Low Temp'))
+        
+        fig.update_layout(title='Daily Temperature Forecast',
+                      xaxis=dict(title='Date'),
+                      yaxis=dict(title='Temperature (°F)'),
+                      barmode='group')
+        
+        return fig
 
         return
     
